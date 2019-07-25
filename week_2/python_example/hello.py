0b1e9555be749c41ca9e6458918fb7b7a59d0548
dhminh1024 <dhminh1024@gmail.com>
2019-07-25 13:08:31 +0700

    Add project milestones

--- week_2/python_example/hello.py
+++ week_2/python_example/hello.py
@@ -6,3 +6,18 @@ cur.execute('SELECT * FROM regions')
 notes = cur.fetchall()
 print(notes)
 conn.close()
+
+
+# Analyze products of Tiki
+# -> Get data from tiki.vn(products, comments, rating, users)
+
+# - Get tikinow and avg rating
+# - Create database
+# - Save products into database
+# - Start Analyzing products data
+# - Get comments and users
+# - Save into database
+# - Find more insights
+# - Prepare for the presentation on MONDAY MORNING: 2 hour
+
+# Good luck guys!!!
